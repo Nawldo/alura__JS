6cbb39be2034f9b9b1f305d38a411908a49db390
Nawldo <nalddu@yahoo.com.b>
2025-05-23 22:36:40 -0300

    Alteracoes

--- main.js
+++ main.js
@@ -4,13 +4,39 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//enquanto
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSom;
-
-    contador = contador + 1;
-
+     const tecla = listaDeTeclas[contador];
+     const instrumento = tecla.classList[1];
+     const idAudio = `#som_${instrumento}`;//template string
+     //console.log(idAudio);
+    
+     tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+    
     console.log(contador);
 }
+
+
+
+
+
+/*let contador = 0;
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+     //console.log(idAudio);
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+
+contador = contador + 1;*/
+
+   //console.log(contador);
+    
